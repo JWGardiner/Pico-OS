f6b2167de054459b604088f00a355db4c2cd76e5
JWGardiner <jamesgardiner340@gmail.com>
2022-07-13 14:57:07 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Pico-OS
-A small operating system for the Pi Pico W
+A small operating system for the Pi Pico W.
+
+This allows you to interact with the hardware in a much easier way, and requires much less time for development for the board.
+
+The structure is simple:
+
+- bin
+- usr
+- Startup
+- main.py
+
+main.py is the main part of the OS.
+
+bin is all of the installed programs.
+
+usr is for configuration.
+
+Upon starting, it will generate these files.
+
+Once you boot it up for the first time, you are greeted by a CLI interface; you can access any commands in /bin and to keep the OS lightweight on the limiting hardware; only a few things are installed.
+
+- Package Manager (WIP)
+- Network Tools
+
+By default the only commands you can use are:
+
+- `pkg`
+- `net-connect`
+- `net-disconnect`
+
+
+# Usage
+
+Files in startup are ran just before the cli starts, which is useful for doing stuff early on.
+
+Files in bin are run when you type the name of the command in the cli.
+
+Files in usr are for configuration.
+
+# Support
+
+WIP
---
+++ main.py
@@ -0,0 +1,96 @@
+import machine
+import network
+import os
+
+
+
+#Setup - Create all the directories, folders etc that are required for normal function
+def setup():
+    led = machine.Pin("LED", machine.Pin.OUT)
+    led.on()
+    try:
+        os.mkdir("bin")
+    except OSError:
+        print("/bin already exists")
+    try:
+        os.mkdir("usr")
+    except OSError:
+        print("/usr already exists")
+    finally:
+        try:
+            f = open("usr/user.cfg", "w")
+            fileSize = os.stat("usr/user.cfg")[6]
+            if fileSize == 0:
+                f.write("default")
+                f.close()
+            else:
+                print("/usr/user.cfg already exists")
+        except OSError:
+            print("/usr/user.cfg already exists")
+            
+        try:
+            os.mkdir("usr/net")
+        except OSError:
+            print("/usr/net already exists")
+        finally:
+            try:
+                f = open("usr/net/ssid.cfg", "w")
+                f.close()
+                fileSize = os.stat("usr/net/ssid.cfg")[6]
+                if fileSize == 0:
+                    f = open("usr/net/ssid.cfg", "w")
+                    f.write("None")
+                    f.close()
+                else:
+                    print("/usr/net/ssid.cfg already exists")
+            except OSError:
+                print("/usr/net/ssid.cfg already exists")    
+            try:
+                f = open("usr/net/pass.cfg", "w")
+                f.close()
+                fileSize = os.stat("usr/net/pass.cfg")[6]
+                if fileSize == 0:
+                    f = open("usr/net/pass.cfg", "w")
+                    f.write("None")
+                    f.close()
+                else:
+                    print("/usr/net/pass.cfg already exists")
+            except OSError:
+                print("/usr/net/pass.cfg already exists")
+    try:
+        os.mkdir("startup")
+    except OSError:
+        print("/startup already exists")    
+    led.off()
+f = open("usr/user.cfg", "r")
+name = f.read()
+f.close()
+
+#Store some important information for later
+class system:
+    led = machine.Pin("LED", machine.Pin.OUT)
+    username = name
+    ver = "0.0.1"
+
+
+#Startup code - Runs all the files in the /startup directory.
+def startup():
+    for file in os.listdir("startup"):
+        try:
+            exec(open("startup/"+file).read())
+        except OSError:
+            print("A startup program encountered an error: \""+file+"\"")
+#Command line interface
+def cli():
+    command = input(name+" $: ")
+    try:
+        exec(open("bin/"+command).read())
+    except OSError:
+        print("Couldn't find command \""+command+"\"")
+    cli()
+    
+#Start the OS
+setup()
+startup()
+print("\nPico OS v"+system.ver)
+cli()
